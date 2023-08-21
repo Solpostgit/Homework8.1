86e3986b1eeebde75268bab697c3ba0e41513038
Solpostgit <begemot_off@mail.ru>
2023-08-21 22:19:16 +0300

    create new branch "hw8.1" and added task2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        task1();/*
-        task2();
+        task1();
+        task2();/*
         task3();
         task4();
         task5();
@@ -17,7 +17,29 @@ public class Main {
         numbersOne[1] = 2;
         numbersOne[2] = 3;
         float[] numbersTwo = {1.57f, 7.654f, 9.986f};
-        String[] abc = {"a", "b", "c", "d", "f"};
+        String[] abc = {"a", "b", "c", "d", "f" };
+        System.out.println();
+    }
+
+    public static void task2() {
+        System.out.println("Task 2");
+        int[] numbersOne = new int[3];
+        numbersOne[0] = 1;
+        numbersOne[1] = 2;
+        numbersOne[2] = 3;
+        for (int i = 0; i < 3; i++) {
+            System.out.print(numbersOne[i] + ", ");
+        }
+        System.out.println();
+        float[] numbersTwo = {1.57f, 7.654f, 9.986f};
+        for (int i = 0; i < 3; i++) {
+            System.out.print(numbersTwo[i] + ", ");
+        }
+        System.out.println();
+        String[] abc = {"a", "b", "c", "d", "e" };
+        for (int i = 0; i < abc.length; i++) {
+            System.out.print(abc[i] + ", ");
+        }
         System.out.println();
     }
 }
